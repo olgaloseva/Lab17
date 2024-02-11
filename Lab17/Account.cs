ec62ab7f3854e44a525b84760878f32318b96576
Ольга Лосева <olgalo@list.ru>
2024-02-11 15:35:04 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    //Класс для моделирования счета в банке, закрытые поля для номера счета, баланса, ФИО владельца.
+    class Account<T>
+    {
+        T NumAccount;
+        float BalanceAccount;
+        string HolderAccount;
+
+        public void Input()
+        {
+            Console.Write("Введите номер счета: ");
+            NumAccount = (T)(Convert.ChangeType(Console.ReadLine(), typeof(T)));
+            Console.Write("Введите баланс: ");
+            BalanceAccount = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите фамилию владельца: ");
+            HolderAccount = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"Номер счета: {NumAccount}, баланс: {BalanceAccount:F2}, владелец: {HolderAccount}";
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        //Создать класс для моделирования счета в банке.Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.
+        //Класс должен быть объявлен как обобщенный.Универсальный параметр T должен определять тип номера счета.
+        //Разработать методы  для доступа  к данным  –  заполнения и  чтения.
+        //Создать несколько экземпляров класса, параметризированного различными типам.Заполнить его поля и вывести на экран информацию об экземпляре класса.
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine();
+            Console.WriteLine(account1.GetInfo());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine();
+            Console.WriteLine(account2.GetInfo());
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
